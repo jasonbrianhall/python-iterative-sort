0ba35003b2aff4d92cf83447472df228140a8a95
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-01-23 12:47:29 -0500

    Latest

--- sort.py
+++ sort.py
@@ -11,43 +11,56 @@
 def sort(data):
 
     array={}
+    array2={}
     counter=0
     for x in data:
         array[counter]=x
         counter+=1
 
-    for x in range(0,len(array),2):
-        temp=array[x]
-        temp2=array[x+1]
-        templen=len(temp)
-        temp2len=len(temp2)
-        i=0
-        j=0
-        buffer=""
-        print("data", temp,temp2)
-        exittheloop=False
-        while exittheloop==False:
-            print(i,j)
-            if i>=templen and j>=temp2len:
-                exittheloop=True
-                print("going 1: ", buffer)
-            elif i>=templen:
-                buffer=buffer+temp2[j]
-                print("Going 2: ", buffer)
-                j+=1
-            elif j>=temp2len:
-                buffer=buffer+temp[i]
-                print("going 3: ", buffer)
-                i+=1
-            elif temp[i]<temp2[j]:
-                buffer=buffer+temp[i]
-                print("going 4:", buffer)
-                i+=1
-            else:
-                buffer=buffer+temp2[j]
-                print("going 5: ", buffer)
-                j+=1
-
+    exittheloop=False
+    while exittheloop==False:
+        counter=0
+        for x in range(0,len(array),2):
+            temp=array.get(x)
+            temp2=array.get(x+1)
+            templen=len(temp)
+            temp2len=len(temp2)
+            i=0
+            j=0
+            buffer=""
+            print("data", temp,temp2)
+            exittheloop2=False
+            while exittheloop2==False and not temp2==None:
+                if i>=templen and j>=temp2len:
+                    exittheloop2=True
+                elif i>=templen:
+                    buffer=buffer+temp2[j]
+                    j+=1
+                elif j>=temp2len:
+                    buffer=buffer+temp[i]
+                    i+=1
+                elif temp[i]<temp2[j]:
+                    buffer=buffer+temp[i]
+                    i+=1
+                else:
+                    buffer=buffer+temp2[j]
+                    j+=1
+            array2[counter]=buffer
+            counter+=1
+        data=[]
+        for x in array:
+            data.append(x)
+        for x in data:
+            del array[x]
+        array=array2.copy()
+        data=[]
+        for x in array2:
+            data.append(x)
+        for x in data:
+            del array2[x]
+        if len(array)==1:
+            exittheloop=True
+    print(array)
 
 data="9876543210986754"
 sort(data)
